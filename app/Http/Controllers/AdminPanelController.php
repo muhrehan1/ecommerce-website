f13bd8c88ce582eaf3d2d6d336b56cae5838179f
muhrehn1 <rehantahir916@gmail.com>
2021-12-05 22:08:28 +0500

    Front-end Integrated

--- app/Http/Controllers/AdminPanelController.php
+++ app/Http/Controllers/AdminPanelController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class AdminPanelController extends Controller
 {
-    //
+    public function index()
+    {
+        $global_titles  = [
+            'title'=>'Ecommerce Website',
+            'description'=>'An Ecommerce Admin Panel'
+
+        ];
+            return view('AdminPanel.dashboard',$global_titles);
+
+    }
 }
